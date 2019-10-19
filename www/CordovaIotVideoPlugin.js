4a9dcb1254d96d4421a66a03fd98c982e7543a59
yagao0o <yagao0o@126.com>
2019-10-19 17:36:50 +0800

    Fix

--- www/CordovaIotVideoPlugin.js
+++ www/CordovaIotVideoPlugin.js
@@ -10,8 +10,8 @@ var CordovaIotVideoPlugin = {
     joinRoom: function (accessToken, roomId, success, error) {
         exec(success, error, 'CordovaIotVideoPlugin', 'joinRoom', [accessToken, roomId]);
     },
-    changeRoom: function (roomId, roomTitle, success, error) {
-        exec(success, error, 'CordovaIotVideoPlugin', 'changeRoom', [roomId, roomTitle]);
+    changeRoom: function (settingConfig, success, error) {
+        exec(success, error, 'CordovaIotVideoPlugin', 'changeRoom', [settingConfig]);
     },
     videoLogout: function (success, error) {
         exec(success, error, 'CordovaIotVideoPlugin', 'videoLogout', []);
